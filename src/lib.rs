e75b6a3653d3a0cfeaef948015dcbc9d3e67508e
Rumen <rumenmitov@protonmail.com>
2024-10-14 07:30:45 +0200

    feat: added TcpStream to womscp conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 pub mod womscp {
+    use std::net::TcpStream;
+    use std::io::Read;
+
+
+    const WOMSCP_LEN :usize = 9;
 
     #[derive(Debug)]
     pub struct WOMSCP {
@@ -9,8 +14,8 @@ pub mod womscp {
         pub data: u32,
     }
 
-    impl From<[u8; 10]> for WOMSCP {
-        fn from(buf: [u8; 10]) -> Self {
+    impl From<[u8; WOMSCP_LEN]> for WOMSCP {
+        fn from(buf: [u8; WOMSCP_LEN]) -> Self {
             WOMSCP { 
                 version: buf[0], 
                 m_id: u16::from_be_bytes([buf[1], buf[2]]),
@@ -18,6 +23,20 @@ pub mod womscp {
                 t: buf[4], 
                 data: u32::from_be_bytes([buf[5], buf[6], buf[7], buf[8]])
             }
+        }
+    }
+
+    impl TryFrom<TcpStream> for WOMSCP {
+        type Error = std::io::Error;
+
+        fn try_from(mut stream: TcpStream) -> Result<Self, Self::Error> {
+            let mut buf :[u8; WOMSCP_LEN] = [0; WOMSCP_LEN];
+
+            if let Err(e) = stream.read(&mut buf) {
+                return Err(e);
+            }
+
+            return Ok(Self::from(buf));
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 use std::net::TcpListener;
-use std::io::Read;
-
 use womscp::womscp::WOMSCP;
 
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
 
     for stream_res in listener.incoming() {
         match stream_res {
-            Ok(mut stream) => {
-                let mut buf :[u8; 10] = [0; 10];
-
-                if let Err(e) = stream.read(&mut buf) {
-                    eprintln!("Could not read from stream error: {:?}", e);
-                } else {
-                    let womscp = WOMSCP::from(buf);
-                    println!("WOMSCP result: {:?}", womscp);
+            Ok(stream) => {
+                match WOMSCP::try_from(stream) {
+                    Ok(res) => { 
+                        dbg!(res);
+                    },
+                    Err(e) => eprintln!("TCP stream read error: {:?}", e),
                 }
             },
             Err(e) => {
