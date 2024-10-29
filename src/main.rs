5eaec1936cb443cdf65e91a21b2e93e4fee1da9e
Rumen Mitov <108842741+rumenmitov@users.noreply.github.com>
2024-10-29 20:41:55 +0100

    misc: restructed the code + using newest implementation of WOMSCP (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
 use std::net::TcpListener;
-use womscp_lib::womscp;
 
+mod connections;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
 
     for stream_res in listener.incoming() {
-        match stream_res {
-            Ok(stream) => {
-                match womscp::Request::try_from(stream) {
-                    Ok(res) => { 
-                        dbg!(res);
-                    },
-                    Err(e) => eprintln!("TCP stream read error: {:?}", e),
-                }
-            },
-            Err(e) => {
-                eprintln!("TCP stream error: {:?}", e);
-            }
-        } 
+        connections::handle_connection(stream_res);
     }
 }
