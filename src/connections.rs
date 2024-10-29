0ac78daa7cd53587cabf9e2107893faf3000d8ad
Rumen Mitov <108842741+rumenmitov@users.noreply.github.com>
2024-10-29 21:24:39 +0100

    Feat/async (#2)
    * feat: adding tokio dependency
    
    * feat: migrating functions to async
    
    * feat: migrated everything to async

--- src/connections.rs
+++ src/connections.rs
@@ -1,34 +1,17 @@
-use std::net::TcpStream;
-use std::io::{Write, Error};
+use tokio::net::TcpStream;
 
-use womscp_lib::womscp::{self, ResponseError};
+use womscp_lib::womscp::{Request, ResponseError};
 
-pub fn handle_connection(stream_res :Result<TcpStream, Error>) {
-        let response :Result<(), ResponseError>;
 
-        match stream_res {
-            Ok(mut stream) => {
-                match womscp::Request::try_from(&stream) {
-                    Ok(req) => { 
-                        dbg!(req);
-                        response = Ok(());
-                    },
-                    Err(res_err) => {
-                        eprintln!("WOMSCP parsing error: {:?}", res_err);
-                        response = Err(res_err)
-                    },
-                }
-
-                if let Err(tcp_err) = match response {
-                    Ok(_) => stream.write(&[0]),
-                    Err(e) => stream.write(&[e as u8])
-                } {
-                    eprintln!("TCP write error: {:?}", tcp_err);
-                }
-            },
-
-            Err(tcp_err) => {
-                eprintln!("TCP stream error: {:?}", tcp_err);
-            }
-        } 
+pub async fn handle_connection(stream :&TcpStream) -> Result<(), ResponseError> {
+    match Request::try_from(stream) {
+        Ok(req) => { 
+            dbg!(req);
+            Ok(())
+        },
+        Err(res_err) => {
+            eprintln!("WOMSCP parsing error: {:?}", res_err);
+            return Err(res_err);
+        },
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use std::net::TcpListener;
+use tokio::net::TcpListener;
 
 mod connections;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
 
-    for stream_res in listener.incoming() {
-        connections::handle_connection(stream_res);
+    loop {
+        if let Ok((stream, _)) = listener.accept().await {
+            let res = connections::handle_connection(&stream)
+                .await;
+
+            if let Err(tcp_err) = match res {
+                Ok(_) => stream.try_write(&[0]),
+                Err(res_err) => stream.try_write(&[res_err as u8])
+            } {
+                eprintln!("TCP write error: {:?}", tcp_err);
+            }
+        }
     }
 }
